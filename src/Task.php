dbe94973e89761323b82ac91229c289c2ee83851
Jordan Johansen & Sam Martinez <student@epicodus.com>
2015-08-17 14:55:24 -0700

    add test find to TaskTest.php

--- src/Task.php
+++ src/Task.php
@@ -58,5 +58,19 @@
             $GLOBALS['DB']->exec("DELETE FROM tasks;");
         }
 
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+
+            foreach($tasks as $task) {
+                $task_id = $task->getId();
+                if ($task_id == $search_id) {
+                    $found_task = $task;
+                }
+            }
+            return $found_task;
+        }
+
     }
  ?>
