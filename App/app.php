e2d3939a4fb4fd05149ca762406308a51c7bbf88
Jordan Johansen & Sam Martinez <student@epicodus.com>
2015-08-17 16:15:09 -0700

    add files

--- App/app.php
+++ App/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
 
@@ -17,6 +18,11 @@
 
     //Route and Controller
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+
+    $app->get("/tasks", function() use ($app) {
 
       $all_tasks = Task::getAll();
 
@@ -24,19 +30,31 @@
 
     });
 
-    //tasks POST
+    //Post Get Methods
+
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+
     $app->post("/tasks", function() use ($app) {
       $task = new Task($_POST['description']);
       $task->save();
       return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
 
     });
-    //tasks POST delete
+
+    //POST delete Methods
     $app->post("/delete_tasks", function() use ($app) {
 
         Task::deleteAll();
 
         return $app['twig']->render('delete_tasks.html.twig');
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
     });
 
     return $app;
